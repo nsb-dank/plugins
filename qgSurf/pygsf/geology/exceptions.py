143024c741f4ee822045ce057ea492f8130c396b
nsb-dank <yukihiko.karata@nsb-dank.com>
2019-05-08 00:32:27 +0900

    other plugins commit

---
+++ qgSurf/pygsf/geology/exceptions.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+class RakeInputException(Exception):
+
+    pass
+
+
+class SlickInputTypeException(Exception):
+    """
+    Exception for slickenline input type.
+    """
+
+    pass
+
+
+class FaultInputTypeException(Exception):
+    """
+    Exception for fault input type.
+    """
+
+    pass
+
+
+class PTBAxesInputException(Exception):
+    """
+    Exception for PTBAxes input
+    """
+
+    pass
+
+
+
+
+
